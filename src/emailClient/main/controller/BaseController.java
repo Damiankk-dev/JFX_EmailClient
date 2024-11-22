863611980f4cf38ce1faee0788689e05e173a34e
damiankk <damian.kaczmarzyk.programista@gmail.com>
2024-11-22 06:30:24 +0100

    Login window action

--- src/emailClient/main/controller/BaseController.java
+++ src/emailClient/main/controller/BaseController.java
@@ -4,8 +4,8 @@ import emailClient.main.EmailManager;
 import emailClient.main.view.ViewFactory;
 
 public abstract class BaseController {
-    private EmailManager emailManager;
-    private ViewFactory viewFactory;
+    protected EmailManager emailManager;
+    protected ViewFactory viewFactory;
     private String fxmlName;
 
     public BaseController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {

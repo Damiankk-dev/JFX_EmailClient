dcd74b318c51315572ca3a60414c0055d40430cc
damiankk <damian.kaczmarzyk.programista@gmail.com>
2024-11-22 06:16:21 +0100

    Load controller by fxml loader

--- src/emailClient/main/view/ViewFactory.java
+++ src/emailClient/main/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package emailClient.main.view;
 
 import emailClient.main.EmailManager;
+import emailClient.main.controller.BaseController;
+import emailClient.main.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
     private EmailManager emailManager;
@@ -11,5 +19,22 @@ public class ViewFactory {
 
     public void showLoginWindow(){
         System.out.println("Show login window");
+
+        BaseController controller = new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try{
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
     }
 }

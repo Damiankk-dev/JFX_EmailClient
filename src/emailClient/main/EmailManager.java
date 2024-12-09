9e0c93e1f77f36495011d6d2f4635d5296588473
damiankk <damian.kaczmarzyk.programista@gmail.com>
2024-12-09 07:47:40 +0100

    Receive a new message

--- src/emailClient/main/EmailManager.java
+++ src/emailClient/main/EmailManager.java
@@ -1,19 +1,35 @@
 package emailClient.main;
 
 import emailClient.main.controller.services.FetchFoldersService;
+import emailClient.main.controller.services.FolderUpdaterService;
 import emailClient.main.model.EmailAccount;
 import emailClient.main.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
+    private FolderUpdaterService folderUpdaterService;
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
     public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
+    private List<Folder> folderList = new ArrayList<Folder>();
+
+    public List<Folder> getFolderList() {
+        return this.folderList;
+    }
+
+    public EmailManager(){
+        folderUpdaterService = new FolderUpdaterService((folderList));
+        folderUpdaterService.start();
+    }
 
     public void addEmailAccount(EmailAccount emailAccount){
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
         treeItem.setExpanded(true);
         foldersRoot.getChildren().add(treeItem);

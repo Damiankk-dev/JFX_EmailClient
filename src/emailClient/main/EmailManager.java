889bc6ce1273c60f8ec3e5d85cfcf6201226c3d8
damiankk <damian.kaczmarzyk.programista@gmail.com>
2024-12-09 07:57:04 +0100

    Make message status read

--- src/emailClient/main/EmailManager.java
+++ src/emailClient/main/EmailManager.java
@@ -3,14 +3,18 @@ package emailClient.main;
 import emailClient.main.controller.services.FetchFoldersService;
 import emailClient.main.controller.services.FolderUpdaterService;
 import emailClient.main.model.EmailAccount;
+import emailClient.main.model.EmailMessage;
 import emailClient.main.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Flags;
 import javax.mail.Folder;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
     private FolderUpdaterService folderUpdaterService;
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
     public EmailTreeItem<String> getFoldersRoot() {
@@ -35,4 +39,29 @@ public class EmailManager {
         foldersRoot.getChildren().add(treeItem);
     }
 
+    public EmailTreeItem<String> getSelectedFolder() {
+        return selectedFolder;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
+
+    public EmailMessage getSelectedMessage() {
+        return selectedMessage;
+    }
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public void setRead() {
+        try{
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessagesCount();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }

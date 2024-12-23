2c856b84303715690e1c359d901acd3396f37f86
damiankk <damian.kaczmarzyk.programista@gmail.com>
2024-12-23 07:41:45 +0100

    Use persistence methods on a start and exit

--- src/emailClient/main/Launcher.java
+++ src/emailClient/main/Launcher.java
@@ -1,5 +1,10 @@
 package emailClient.main;
 
+import emailClient.main.controller.EmailSendingResult;
+import emailClient.main.controller.persistence.PersistenceAccess;
+import emailClient.main.controller.persistence.ValidAccount;
+import emailClient.main.controller.services.LoginService;
+import emailClient.main.model.EmailAccount;
 import emailClient.main.view.ViewFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,15 +12,43 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Launcher extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
+    private PersistenceAccess persistenceAccess = new PersistenceAccess();
+    private EmailManager emailManager = new EmailManager();
     @Override
     public void start(Stage stage) throws Exception {
-        ViewFactory viewFactory = new ViewFactory(new EmailManager());
-        viewFactory.showLoginWindow();
-        viewFactory.updateStyles();
+        ViewFactory viewFactory = new ViewFactory(emailManager);
+        List<ValidAccount> validAccountList = persistenceAccess.loadFromPersistence();
+        if(validAccountList.size()>0){
+            viewFactory.showMainWindow();
+            for (ValidAccount validAccount: validAccountList){
+                EmailAccount emailAccount = new EmailAccount(validAccount.getAddress(), validAccount.getPassword());
+                LoginService loginService = new LoginService(emailAccount, emailManager);
+                loginService.start();
+            }
+        } else {
+            viewFactory.showLoginWindow();
+        }
     }
+
+    private void checkPersistence() {
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+        List<ValidAccount> validAccountList = new ArrayList<ValidAccount>();
+        for (EmailAccount emailAccount : emailManager.getEmailAccounts()){
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistenceAccess.saveToPersistence(validAccountList);
+    }
+
 }
--- src/emailClient/main/controller/persistence/PersistenceAccess.java
+++ src/emailClient/main/controller/persistence/PersistenceAccess.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersistenceAccess {
-    private String VALID_ACCOUNTS_LOCATION = System.getenv("APPDATA") + "\\validAccounts.ser";
+    private String VALID_ACCOUNTS_LOCATION = System.getProperty("user.home") + File.separator + "validAccounts.ser";
     public List<ValidAccount> loadFromPersistence(){
         List<ValidAccount> resultList = new ArrayList<ValidAccount>();
         try {
@@ -23,6 +23,7 @@ public class PersistenceAccess {
             File file = new File(VALID_ACCOUNTS_LOCATION);
             FileOutputStream fileOutputStream = new FileOutputStream(file);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(validAccounts);
             objectOutputStream.close();
             fileOutputStream.close();
         } catch (Exception e) {
